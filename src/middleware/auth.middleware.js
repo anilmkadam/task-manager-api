2b67e1f57799cc4474353a7f1c9ff20b9f9bb56a
Anil Kadam <anilkadam210@gmail.com>
2021-06-11 18:51:46 +0530

    feat: authorization middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const { rawListeners } = require('../models/user');
+const User = require('../models/user');
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace("Bearer ", "");
+        const decodedToken = jwt.verify(token, "thisismycourse");
+
+        const user = await User.findOne({_id: decodedToken._id, 'tokens.token': token});
+        if(!user)
+            throw new Error();
+        req.token = token;
+        req.user = user;
+        next();
+    } catch(err) {
+        res.send("Please Authenticate!")
+    }
+}
+
+module.exports = auth;
